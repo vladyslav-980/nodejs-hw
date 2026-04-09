e01b44cc048448d497b097d000a58632cd6a5a1b
Vladyslav Huminiuk <vldgum@gmail.com>
2026-04-09 17:45:17 +0300

    fix-noteId

--- src/server.js
+++ src/server.js
@@ -37,7 +37,8 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/notes/:noteId', (req, res) => {
-  res.status(200).json({ "message": "Retrieved note with ID: id_param"});
+  const { noteId } = req.params;
+  res.status(200).json({ "message": `Retrieved note with ID: ${noteId}` });
 });
 
 app.get('/test-error', (req, res) => {
