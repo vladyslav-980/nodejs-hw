33a6d96bba701c40b113ea2d04bb36cd5e8c995b
Vladyslav Huminiuk <vldgum@gmail.com>
2026-05-26 22:17:17 +0300

    fix validation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -14,8 +14,8 @@ export const updateNoteSchema = {
   }),
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).max(30),
-    content: Joi.string().min(0).max(65),
-    tag: Joi.string().valid(...TAGS),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string().valid(...TAGS).optional(),
   }).min(1),
 };
 
@@ -23,8 +23,8 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
-    search: Joi.string().min(0).max(65)
+    tag: Joi.string().valid(...TAGS).optional(),
+    search: Joi.string().max(65).optional(),
   }),
 };
 
@@ -36,22 +36,16 @@ export const noteIdSchema = {
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).max(30).required().messages({
+    title: Joi.string().min(1).required().messages({
       "string.base": "Title must be a string",
       "string.min": "Title should have at least {#limit} characters",
       "string.max": "Title should have at most {#limit} characters",
       "any.required": "Title is required",
     }),
-    content: Joi.string().min(0).max(65).messages({
-      "string.base": "Content must be a string",
-      "string.min": "Content should have at least {#limit} characters",
-      "string.max": "Content should have at most {#limit} characters",
-      "any.required": "Content is required",
-    }),
-    tag: Joi.string().valid(...TAGS).messages({
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string().valid(...TAGS).optional().messages({
       "string.base": "Tag must be a string",
-      "string.valid": "Tag must be one of the allowed values",
-      "any.required": "Tag is required",
+      "any.only": "Tag must be one of the allowed values",
     }),
   }),
 };
